457204f419d822e55fd3f089d360dc1583f8c3bb
jnpblrg <jnpbl.rg@gmail.com>
2013-03-29 17:27:15 +0100

    palometa baca

--- palometa_baca.cpp
+++ palometa_baca.cpp
@@ -13,19 +13,21 @@ int main()
 
     radio = 22;
     alto = 20;
-    diam_tuerca = 11.5;
+    diam_tuerca = 10.8;
     diam_tornillo = 7;
 
     int caras;
 
     caras = 6;
 
+
     Component palometabaca = Cylinder(radio,alto)
             - Cylinder(radio+1,alto+1).translate(0,-(radio+13),0) //abajo
             - Cylinder(radio+1,alto+1).translate(0,-(radio+13),0).rotate(0,0,120) //derecho
             - Cylinder(radio+1,alto+1).translate(0,-(radio+13),0).rotate(0,0,-120) //izquierdo
-            - Cylinder(diam_tuerca/2,alto,caras,true).translate(0,0,5)
+            - Cylinder(diam_tuerca/2,diam_tuerca/2+2,alto,caras,true).translate(0,0,5)
             - Cylinder(diam_tornillo/2,alto+1)
+            - Cylinder(diam_tuerca/2+2,alto).translate(0,0,alto-4) //cilindro arriba
 
             ;
 
